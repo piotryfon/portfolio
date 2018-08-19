ee2eccfaed07bd43ef51ec4271acfdd64daadf90
piotryfon <pit.pit@wp.pl>
2018-08-19 19:13:19 +0200

    losowanie lotka

---
+++ losowanie/losowanie.js
@@ -0,0 +1,35 @@
+(function(){
+
+    let btn = document.querySelector("#getNumbers"),
+        output = document.querySelector("#showNumbers");
+
+    function getRandom(min, max){
+        return Math.round(Math.random() * (max - min) + min)
+    }
+
+    function sortNumbers(a,b){
+        if(a<b) return -1
+        else if(a>b) return 1
+        else return 0
+    }
+
+    function showNumbers(){
+        let numbers = [],
+            rand;
+            
+        for(let i = 0; i < 6; i++){
+
+            rand = getRandom(1, 49);
+            
+            while(numbers.indexOf(rand) !== -1){
+                rand = getRandom(1, 49);
+            }
+
+            numbers.push(rand);  
+        }
+        let sortedNumbers = numbers.sort(sortNumbers)
+        output.value = sortedNumbers.join(", ")
+    }
+    btn.onclick = showNumbers;
+
+})();

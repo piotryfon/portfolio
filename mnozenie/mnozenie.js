8855ff91aa312f1b2c35f96148690ce756e7f173
piotryfon <pit.pit@wp.pl>
2018-09-25 20:13:52 +0200

    jQuery in mnozenie js

--- mnozenie/mnozenie.js
+++ mnozenie/mnozenie.js
@@ -1,51 +1,52 @@
-let input = document.querySelector("#input");
-let showTask = document.querySelector("#showTask");
-let showResultDiv = document.querySelector("#show-result-div");
-let start = document.querySelector("#start");
-start.addEventListener("click", randomizeTask);
-let checkBtn = document.querySelector("#check-btn");
-checkBtn.addEventListener("click", submitVal);
-let parsedValue;
-let result;
+$(document).ready(function(){
 
-function getRandomNumberInRange(min, max) {
-    return Math.round(Math.random() * (max - min)) + min;
-}
-
-function randomizeTask() {
-
-    let a = getRandomNumberInRange(1, 10);
-    let b = getRandomNumberInRange(1, 10);
-
-    result = a * b;
-    console.log("Wynik to: " + result)
-    showTask.innerHTML = `${a} x ${b} =`
-    start.removeEventListener("click", randomizeTask);
-    checkBtn.addEventListener("click", submitVal);
-    showResultDiv.innerHTML = "<h3>Dasz radę!</h3>";
-}
-
-function submitVal() {
-
-    if (input.value) {
-        parsedValue = parseInt(input.value);
-        console.log("Napisałeś: " + parsedValue);
-        checkResult();
-    } else {
-        showResultDiv.innerHTML = `<h3>Naciśnij "Losuj zadanie" i wpisz wynik!</h3>`
+    let input = $("#input");
+    let showTask = $("#showTask");
+    let showResultDiv = $("#show-result-div");
+    let start = $("#start");
+    start.on("click", randomizeTask);
+    let checkBtn = $("#check-btn");
+    checkBtn.on("click", submitVal);
+    let parsedValue;
+    let result;
+    
+    function getRandomNumberInRange(min, max) {
+        return Math.round(Math.random() * (max - min)) + min;
     }
-}
-
-function checkResult() {
-
-    console.log("result: " + result + " value: " + parsedValue)
-    console.log(result === parsedValue)
-    if (result === parsedValue) {
-        showResultDiv.innerHTML = "<h3>Bardzo dobrze!</h3>"
-    } else {
-        showResultDiv.innerHTML = `<h3>Źle, wynik to: ${result}</h3>`
+    
+    function randomizeTask() {
+    
+        let a = getRandomNumberInRange(1, 10);
+        let b = getRandomNumberInRange(1, 10);
+    
+        result = a * b;
+        showTask.html( a + " X " + b + " = ");
+        start.off("click", randomizeTask);
+        checkBtn.on("click", submitVal);
+        showResultDiv.html("<h3>Dasz radę!</h3>");
     }
+    
+    function submitVal() {
+    
+        if (input.val()) {
+            parsedValue = parseInt(input.val());
+            checkResult();
+        } else {
+            showResultDiv.html('<h3>Naciśnij "Losuj zadanie" i wpisz wynik!</h3>');
+        }
+    }
+    
+    function checkResult() {
+    
+        if (result === parsedValue) {
+            showResultDiv.html("<h3>Bardzo dobrze!</h3>");
+        } else {
+            showResultDiv.html("<h3>Źle, wynik to: " + result + "</h3>");
+        }
+    
+        start.on("click", randomizeTask);
+        checkBtn.off("click", submitVal);
+    }
+});
 
-    start.addEventListener("click", randomizeTask);
-    checkBtn.removeEventListener("click", submitVal);
-}
+

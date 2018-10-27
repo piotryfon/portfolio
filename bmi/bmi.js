edd144d8ade2e6ae4c41ab76a6fd79319596bd70
piotryfon <pit.pit@wp.pl>
2018-10-27 21:55:02 +0200

    table in bmi

--- bmi/bmi.js
+++ bmi/bmi.js
@@ -24,22 +24,22 @@ $(document).ready(function() {
         if(h && kg) {
             result = kg / (h * h);
             let roundResult = Math.decimal(result, 2);
-            console.log("BMI: " + roundResult)
+            
             if(roundResult < 16) {
-                $("output").html("<h3> Wygłodzenie. </h3>");
+                $("output").html("<h3>bmi " + roundResult + " - wygłodzenie. </h3>");
             } else if (roundResult >= 16 && roundResult < 17) {
-                $("output").html("<h3> Wychudzenie. </h3>");
+                $("output").html("<h3>bmi " + roundResult + " - wychudzenie. </h3>");
             } else if (roundResult >= 17 && roundResult < 18.5) {
-                $("output").html("<h3> Niedowaga. </h3>");
+                $("output").html("<h3>bmi " + roundResult + " - niedowaga. </h3>");
             } else if(roundResult >=18.5 && roundResult < 25) {
-                $("output").html("<h3> Wartość bmi prawidłowa. </h3>");
+                $("output").html("<h3>bmi " + roundResult + " - wartość bmi prawidłowa. </h3>");
             } else if(roundResult >= 25 && roundResult < 30) {
-                $("output").html("<h3> Nadwaga. </h3>");
+                $("output").html("<h3>bmi " + roundResult + " - nadwaga. </h3>");
             } else if(roundResult >= 30 && roundResult < 35) {
-                $("output").html("<h3> I stopień otyłości. </h3>");
+                $("output").html("<h3>bmi " + roundResult + " - I stopień otyłości. </h3>");
             } else if(roundResult >= 35 && roundResult < 40) {
-                $("output").html("<h3> II stopień otyłości (otyłość kliniczna).</h3>");
-            } else $("output").html("<h3> III stopień otyłości (otyłość skrajna).</h3>");
+                $("output").html("<h3>bmi " + roundResult + " - II stopień otyłości (otyłość kliniczna).</h3>");
+            } else $("output").html("<h3>bmi " + roundResult + " - III stopień otyłości (otyłość skrajna).</h3>");
         } else $("output").html("<h3> Uzupełnij wartości w polach powyżej. </h3>");
     })
     

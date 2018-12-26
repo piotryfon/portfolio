9679ab911a5967f508a4ccad2b70f3e6718a7af1
piotryfon <pit.pit@wp.pl>
2018-12-26 10:52:12 +0100

    try to scroll on mobile devices

--- losowanie/losowanie.js
+++ losowanie/losowanie.js
@@ -1,35 +1,36 @@
-(function(){
-    $(document).ready(function(){
+(function () {
+    $(document).ready(function () {
+
         let btn = $("#getNumbers"),
-        output = $("#showNumbers");
+            output = $("#showNumbers");
 
-    function getRandom(min, max){
-        return Math.round(Math.random() * (max - min) + min)
-    }
-
-    function sortNumbers(a,b){
-        if(a < b) return -1
-        else if(a > b) return 1
-        else return 0
-    }
-
-    function showNumbers(){
-        let numbers = [],
-            rand;
-            
-        for(let i = 0; i < 6; i++){
-
-            rand = getRandom(1, 49);
-            
-            while(numbers.indexOf(rand) !== -1){
-                rand = getRandom(1, 49);
-            }
-
-            numbers.push(rand);  
+        function getRandom(min, max) {
+            return Math.round(Math.random() * (max - min) + min)
         }
-        let sortedNumbers = numbers.sort(sortNumbers)
-        output.html(sortedNumbers.join(", "));
-    }
-    btn.on("click", showNumbers);
+
+        function sortNumbers(a, b) {
+            if (a < b) return -1
+            else if (a > b) return 1
+            else return 0
+        }
+
+        function showNumbers() {
+            let numbers = [],
+                rand;
+
+            for (let i = 0; i < 6; i++) {
+
+                rand = getRandom(1, 49);
+
+                while (numbers.indexOf(rand) !== -1) {
+                    rand = getRandom(1, 49);
+                }
+
+                numbers.push(rand);
+            }
+            let sortedNumbers = numbers.sort(sortNumbers)
+            output.html(sortedNumbers.join(", "));
+        }
+        btn.on("click", showNumbers);
     });
 })();
